709eb50d9ef4a352d3661b378660a04d8415ba06
IvanGonzalezHernandez <ivangonzalez.code@gmail.com>
2025-07-07 17:00:11 +0200

    Añadiendo scripts sql, SecurityConfig y WebConfig

--- src/main/java/com/ashyaart/ashya_art_backend/controller/CursoController.java
+++ src/main/java/com/ashyaart/ashya_art_backend/controller/CursoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import com.ashyaart.ashya_art_backend.filter.CursoFilter;
 import com.ashyaart.ashya_art_backend.model.CursoDto;
 import com.ashyaart.ashya_art_backend.service.CursoService;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/cursos")
 public class CursoController {
