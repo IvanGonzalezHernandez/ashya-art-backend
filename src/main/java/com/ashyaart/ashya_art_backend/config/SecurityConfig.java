2be390c3547c855d41962fbee855684d602be652
IvanGonzalezHernandez <ivangonzalez.code@gmail.com>
2025-07-07 20:55:55 +0200

    Arreglando CORS

--- src/main/java/com/ashyaart/ashya_art_backend/config/SecurityConfig.java
+++ src/main/java/com/ashyaart/ashya_art_backend/config/SecurityConfig.java
@@ -28,7 +28,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+        	    "http://localhost:4200",
+        	    "https://ashya-art-frontend.onrender.com"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("*"));
